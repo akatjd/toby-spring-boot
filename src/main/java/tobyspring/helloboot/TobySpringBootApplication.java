79509976c7fbf72f96fbd61f4cd52b2f7cd0f27d
akatjd <akatjd11@gmail.com>
2023-01-25 19:32:43 +0900

    섹션 4. 독립 실행형 스프링 애플리케이션 - '스프링 컨테이너 사용' 까지

--- src/main/java/tobyspring/helloboot/TobySpringBootApplication.java
+++ src/main/java/tobyspring/helloboot/TobySpringBootApplication.java
@@ -1,13 +1,97 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class TobySpringBootApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringBootApplication.class, args);
-    }
+        // 추상화 시켜놔서 톰캣말고 제티 등의 다른 서블릿 컨테이너도 지원 가능 (동일하게 동작)
+        // 빈 컨테이너 서블릿 실행 (톰캣 띄우고 HelloController get request 날려도 404에러 뜸)
+        // 독립 실행이 가능한 Servlet application
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
+//        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+//            @Override
+//            public void onStartup(ServletContext servletContext) throws ServletException {
+//
+//            }
+//        });
+
+        // lamda 변환
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+//            servletContext.addServlet("hello", new HttpServlet() {
+//                @Override
+//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                    String name = req.getParameter("name");
+//                    // spring enum 사용
+////                    resp.setStatus(200);
+//                    resp.setStatus(HttpStatus.OK.value());
+////                    resp.setHeader("Content-Type", "text/plain");
+//                    // spring enum 사용
+//                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                    resp.getWriter().println("Hello Servlet ~ Hello :: " + name);
+//                }
+//            }).addMapping("/hello");
+
+            // front controller 만들기
+//            servletContext.addServlet("frontcontroller", new HttpServlet() {
+//                HelloController helloController = new HelloController();
+//                @Override
+//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                    // 인증, 보안, 다국어, 공통 기능
+//                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+//                        String name = req.getParameter("name");
+//                        String ret = helloController.hello(name);
+//                        resp.setStatus(HttpStatus.OK.value());
+//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                        resp.getWriter().println(ret);
+//                    }else if(req.getRequestURI().equals("/user")) {
+//                        // user 로직 실행
+//                    }else {
+//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+//                    }
+//
+//                }
+//            }).addMapping("/*");
+
+            GenericApplicationContext applicationContext = new GenericApplicationContext();
+            applicationContext.registerBean(HelloController.class);
+            applicationContext.refresh();
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
+                        String ret = helloController.hello(name);
+                        // 안넣으면 기본적으로 200으로 날아가니 생략 가능
+//                        resp.setStatus(HttpStatus.OK.value());
+//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        // content type도 아래와 같이 줄일 수 있음
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+                    }else if(req.getRequestURI().equals("/user")) {
+                        // user 로직 실행
+                    }else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
+                }
+            }).addMapping("/*");
+        });
+        webServer.start();
+    }
 }
